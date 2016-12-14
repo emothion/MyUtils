a5ec1e97e2e874743bb30da854a4a55bdab3254c
emothion <emothion@DESKTOP-OCQ2U59>
2016-12-14 09:48:08 +0800

    加注释

--- src/com/emothion/hashmap/mytools/MapUtils.java
+++ src/com/emothion/hashmap/mytools/MapUtils.java
@@ -1,5 +1,16 @@
 package com.emothion.hashmap.mytools;
 
+/**
+ * @ClassName: com.emothion.hashmap.mytools.MapUtils.java
+ * @Description: HashMap工具类
+ * <table border=1>
+ * <tr><th>时间</th><th>修改内容</th></tr>
+ * <tr><td>2016年12月14日</td><td>Description</td></tr>
+ * </table>
+ * @version: v1.0.0
+ * @author: emothion
+ * @date: 2016年12月14日 上午9:40:35
+ */
 public class MapUtils {
 
 }
